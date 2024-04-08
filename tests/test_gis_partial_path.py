d4a5680cae5d80b65cf43eb728533d84bf3fe857
antigravkids <hcarter333@gmail.com>
2024-04-07 21:33:39 -0700

    Getting rid of incomplete input message

--- tests/test_gis_partial_path.py
+++ tests/test_gis_partial_path.py
@@ -16,12 +16,12 @@ def conn():
 @pytest.mark.parametrize("type", (float, str))
 def test_gis_partial_path(conn, type):
     actual = conn.execute(
-        "select gis_partial_path_lat((?, ?, ?, ?, ?)",
+        "select gis_partial_path_lat(?, ?, ?, ?, ?)",
         [type(KD0FNR[0]), type(KD0FNR[1]), type(N6MKW[0]), type(N6MKW[1]), 2000],
     ).fetchall()[0][0]
     assert el_lat == pytest.approx(actual, rel=1e-2)
     actual = conn.execute(
-        "select gis_partial_path_lng((?, ?, ?, ?, ?)",
+        "select gis_partial_path_lng(?, ?, ?, ?, ?)",
         [type(KD0FNR[0]), type(KD0FNR[1]), type(N6MKW[0]), type(N6MKW[1]), 2000],
     ).fetchall()[0][0]
     assert el_lng == pytest.approx(actual, rel=1e-2)
